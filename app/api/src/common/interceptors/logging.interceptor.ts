f91a7357b210a9bc3817c6f99524aa34a6b2a502
Fico Seitún <fseitun@gmail.com>
2024-11-13 17:57:37 -0300

    chore: add debug payload

--- app/api/src/common/interceptors/logging.interceptor.ts
+++ app/api/src/common/interceptors/logging.interceptor.ts
@@ -11,22 +11,23 @@ import { HttpException } from "@nestjs/common";
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   private readonly logger = new Logger(LoggingInterceptor.name);
+  private readonly debug = true;
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
     const { method, url, body } = request;
 
-    this.logger.log(`Incoming Request: ${method} ${url} ${JSON.stringify(body)}`);
-
     const now = Date.now();
     return next.handle().pipe(
-      tap((data) =>
-        this.logger.log(
-          `Response: ${method} ${url} ${JSON.stringify(data)} - ${
-            Date.now() - now
-          }ms`
-        )
-      ),
+      tap((data) => {
+        if (this.debug) {
+          this.logger.log(
+            `Response: ${method} ${url} ${
+              this.debug ? JSON.stringify(data, null, 2) : ""
+            } - ${Date.now() - now}ms`
+          );
+        }
+      }),
       catchError((err: HttpException) => {
         this.logger.error(
           `Error Response: ${method} ${url} ${err.getStatus()} - ${
